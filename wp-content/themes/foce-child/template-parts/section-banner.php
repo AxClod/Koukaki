4a7857200a0975764fd0d9f2751171629d522ab1
Axel Clody <axelclody@gmail.com>
2024-11-02 16:21:55 +0100

    correction swipper pour intégrer la boucle wordpress + correction responsive

--- wp-content/themes/foce-child/template-parts/section-banner.php
+++ wp-content/themes/foce-child/template-parts/section-banner.php
@@ -1,6 +1,6 @@
 <section class="banner fadeIn fadeInverse">
 <video class="banner_video" poster="<?php echo get_theme_file_uri() . '/assets/images/banner-image.png'; ?>" autoplay muted loop>
-        <source src=<?php echo get_theme_file_uri() . '/assets/video/Studio+Koukaki-vidéo.mp4'; ?> type="video/mp4">
+        <source src=<?php echo get_theme_file_uri() . '/assets/video/Studio+Koukaki-video.mp4'; ?> type="video/mp4">
     </video>
     <img class="banner_logo" src="<?php echo get_theme_file_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants">
 </section>
--- wp-content/themes/foce-child/template-parts/swiper-characters.php
+++ wp-content/themes/foce-child/template-parts/swiper-characters.php
@@ -1,20 +1,25 @@
 <div class="swiper fadeIn">
     <div class="swiper-wrapper">
-        <div class="swiper-slide">
-            <img src="<?php echo get_theme_file_uri('/assets/images/Kawaneko.png'); ?>">
-        </div>
-        <div class="swiper-slide">
-            <img src="<?php echo get_theme_file_uri('/assets/images/orenjiiro.png'); ?>">
-        </div>
-        <div class="swiper-slide">
-            <img src="<?php echo get_theme_file_uri('/assets/images/pinku.png'); ?>">
-        </div>
-        <div class="swiper-slide">
-            <img src="<?php echo get_theme_file_uri('/assets/images/tenshi.png'); ?>">
-        </div>
-        <div class="swiper-slide">
-            <img src="<?php echo get_theme_file_uri('/assets/images/jaakuna.png'); ?>">
-        </div>
-        <div class="swiper-pagination"></div>
+        <?php
+        $args = array(
+            'post_type' => 'characters',
+            'posts_per_page' => -1,
+            'meta_key'  => '_main_char_field',
+            'orderby'   => 'meta_value_num',
+        );
+        $characters_query = new WP_Query($args);
+        
+        if ($characters_query->have_posts()) :
+            while ($characters_query->have_posts()) : $characters_query->the_post();
+                ?>
+                <div class="swiper-slide">
+                    <?php echo get_the_post_thumbnail(get_the_ID(), 'full'); ?>
+                    <figcaption><?php the_title(); ?></figcaption>
+                </div>
+                <?php
+            endwhile;
+            wp_reset_postdata(); // Remettre les données de requête par défaut
+        endif;
+        ?>
     </div>
 </div>
